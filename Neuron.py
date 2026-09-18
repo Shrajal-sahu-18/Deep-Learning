4f33f8092b574c9dd9838a65e3f202f6977cf90c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-18 22:38:57 +0530

    output

--- Neuron.py
+++ Neuron.py
@@ -4,4 +4,6 @@ import torch.nn as nn
 #Input
 input = torch.tensor([1.0,2.0,3.0])
 
-neuron = nn.Linrea(in_features = 3 , out_features = 1)
+neuron = nn.Linear(in_features = 3 , out_features = 1)
+
+output = neuron(input)
