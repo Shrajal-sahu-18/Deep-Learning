68ddc3f2d31b39cc1aa4481a7ef6ea91d321da4c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-21 20:29:56 +0530

    convert testing data into tensor

--- ANN.Regressor.py
+++ ANN.Regressor.py
@@ -26,3 +26,5 @@ import torch.nn as nn
 
 X_train_tensor = torch.tensor(X_train_scaled , dtype = torch.float32)
 X_test_tensor = torch.tensor(X_test_scaled , dtype = torch.float32)
+y_train_tensor = torch.tensor(y_train.values,dtype = torch.float32).view(-1,1)
+y_test_tensor = torch.tensor(y_test.values ,dtype = torch.float32).view(-1,1)
