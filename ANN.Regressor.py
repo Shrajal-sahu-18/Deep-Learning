efde7f8524cd3b18eab761f8f460b02b059bbc26
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-21 20:32:03 +0530

    training data convert from tensordataset

--- ANN.Regressor.py
+++ ANN.Regressor.py
@@ -30,3 +30,4 @@ y_train_tensor = torch.tensor(y_train.values,dtype = torch.float32).view(-1,1)
 y_test_tensor = torch.tensor(y_test.values ,dtype = torch.float32).view(-1,1)
 
 from torch.utils.data import TensorDataset, DataLoader
+train_dataset = TensorDataset(X_train_tensor,y_train_tensor)
