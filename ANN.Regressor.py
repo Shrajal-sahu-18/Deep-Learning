16ff0ca65806e8e7bc7d139d5d2dd664d564c87f
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-21 20:27:57 +0530

    scale the data

--- ANN.Regressor.py
+++ ANN.Regressor.py
@@ -17,3 +17,6 @@ X_train,X_test,y_train,y_test = train_test_split(
 )
 
 from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
