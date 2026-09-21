c939e1cf2751f6d9c640986ee55b34227f8d55ef
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-21 20:25:46 +0530

    split the data using train test split

--- ANN.Regressor.py
+++ ANN.Regressor.py
@@ -12,3 +12,6 @@ y = df["PE"]
 
 # Split the data
 from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(
+    X,y,test_size = 0.2, random_state = 42
+)
