49e184fccb2bd813926e6d010320cbe05d778b2c
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-21 20:28:34 +0530

    import torch

--- ANN.Regressor.py
+++ ANN.Regressor.py
@@ -20,3 +20,6 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
+import torch
+import torch.nn as nn
