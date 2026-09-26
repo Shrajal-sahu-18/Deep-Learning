f25d36eadfee182ba11e6e8f2c2c803b2f7efb65
shrajal-sahu-18 <shrajalsahu169@gmail.com>
2026-09-26 14:42:30 +0530

    Scale trainning data

--- Ann/ANN_Classifier.py
+++ Ann/ANN_Classifier.py
@@ -17,3 +17,7 @@ from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(
     X,y,test_size = 0.2,random_state = 42
 )
+
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
